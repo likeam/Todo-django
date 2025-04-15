d5c82c5e05f7f8bdb2b10f428e8010e47c7e094a
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2025-04-15 16:10:03 +0500

    Buiding signin

---
+++ tododg/tododg/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from tododg.models import TODOO
+
+admin.site.register(TODOO)
--- tododg/tododg/views.py
+++ tododg/tododg/views.py
@@ -1,5 +1,25 @@
 from django . shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from tododg import models
+from tododg.models import TODOO
 
 
 def signup(request):
+    if request.method == 'POST':
+        fnm=request.POST.get('fnm')
+        emailid=request.POST.get('emailid')
+        pwd=request.POST.get('pwd')
+        print(fnm, emailid, pwd)
+        my_user=User.objects.create_user(fnm, emailid, pwd)
+        my_user.save()
+        return redirect('/login')
     return render(request, 'signup.html')
+
+
+def login(requset):
+    if requset.method == 'POST':
+        fnm=requset.POST.get('fnm')
+        pwd=requset.POST.get('pwd')
+        print(fnm, pwd)
+        userr=authenticate(requset,username=fnm,password=pwd)
